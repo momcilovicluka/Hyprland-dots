ade5dc4e5bd3e5bf7719e5fd739d906db41b50b1
Luka Momčilović <l.momcilovic61@gmail.com>
2023-12-09 21:50:33 +0100

    Fix waybar upgradable packages and modify wlogout

--- hypr/scripts/systemupdate.sh
+++ hypr/scripts/systemupdate.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
 # Check for updates
+#yay -Syy
+#pacman -Syy
+
 aur=`yay -Qu | wc -l`
+ofc=`pacman -Qu | wc -l`
 
 # Calculate total available updates
-upd=$(( aur ))
+upd=$(( aur + pac ))
 
 # Show tooltip
 if [ $upd -eq 0 ] ; then
     echo "{\"text\":\"$upd\", \"tooltip\":\" Packages are up to date\"}"
 else
-    echo "{\"text\":\"$upd\", \"tooltip\":\"󱓽 Official $ofc\n󱓾 AUR $aur$fpk_disp\"}"
+    echo "{\"text\":\"$upd\", \"tooltip\":\"󱓽 Pacman $ofc\n󱓾 AUR $aur$fpk_disp\"}"
 fi
 
 # Trigger upgrade
 if [ "$1" == "up" ] ; then
-    kitty --title systemupdate sh -c "yay -Syu"
+    #kitty --title systemupdatepacman sh -c "pacman -Syu"
+    kitty --title systemupdateyay sh -c "yay -Syu"
 fi
