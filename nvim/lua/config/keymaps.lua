000f2aabc0bf73bed99405f502082ce7f6d46479
Luka Momčilović <l.momcilovic61@gmail.com>
2024-03-17 17:22:24 +0100

    feat(nvim): add blank line keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- add blank line above and below with alt o and alt O
+vim.api.nvim_set_keymap("n", "<A-h>", "O<ESC>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-l>", "o<ESC>k", { noremap = true, silent = true })
