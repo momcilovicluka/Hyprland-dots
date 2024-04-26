5bd2d509e47eae32930f3a78971308fcd43ecdea
Luka Momčilović <l.momcilovic61@gmail.com>
2024-04-26 23:58:10 +0200

    feat(nvim): add better java support

---
+++ nvim/lua/plugins/nvim-java.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-java/nvim-java",
+    enabled = false,
+    dependencies = {
+        "nvim-java/lua-async-await",
+        "nvim-java/nvim-java-core",
+        "nvim-java/nvim-java-test",
+        "nvim-java/nvim-java-dap",
+        "MunifTanjim/nui.nvim",
+        "neovim/nvim-lspconfig",
+        "mfussenegger/nvim-dap",
+        {
+            "williamboman/mason.nvim",
+            opts = {
+                registries = {
+                    "github:nvim-java/mason-registry",
+                    "github:mason-org/mason-registry",
+                },
+            },
+        },
+    },
+}
